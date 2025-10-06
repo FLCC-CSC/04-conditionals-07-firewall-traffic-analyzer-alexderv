d559d2e1e0643d35f3ca896092bb06c208a059bc
Alex <66220940+alexderv@users.noreply.github.com>
2025-10-06 00:20:13 +0000

    firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,7 +1,7 @@
 # FILE NAME - firewall_traffic_analyzer.py
 
-# NAME: 
-# DATE: 
+# NAME: Alex Dervenis
+# DATE: 10/5
 # BRIEF DESCRIPTION:  
 
 
@@ -15,6 +15,26 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+def main():
+    def firewall_analyzer():
+        print('=== Network Traffic Security Analyzer ===')
+        print()
+        port_number = int(input('Enter the port number(e.g., 80, 22, 443, 3389): '))
+        transfer_size = int(input('Enter the data transfer size in megabytes (MB): '))
+        print()
+        print('FIREWALL LOG:')
+        print(f'Port: {port_number}, Transfer Size: {transfer_size} MB')
+        if port_number == 22 or port_number == 3389 and transfer_size >= 100:
+            print('Risk Assessment: HIGH RISK: Potential unauthorized remote access detected!')
+        elif port_number == 80 and transfer_size > 100:
+            print('Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.')
+        elif port_number == 443:
+            print('Risk Assessment: LOW RISK: Secure encrypted transfer detected.')
+        else:
+            print('Risk Assessment: UNKNOWN: Unrecognized traffic pattern.')
+        print('-' * 24)    
+    firewall_analyzer()
+main()    
 
 
 
